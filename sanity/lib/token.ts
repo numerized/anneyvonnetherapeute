0c10aaafbccd2f8576aa31f26a07c624ecd07026
kev <numerized@users.noreply.github.com>
2024-11-27 04:22:01 +0100

    token security

--- sanity/lib/token.ts
+++ sanity/lib/token.ts
@@ -1,15 +1,12 @@
 import 'server-only'
 
-import { experimental_taintUniqueValue } from 'react'
-
 export const token = process.env.SANITY_API_READ_TOKEN
 
 if (!token) {
   throw new Error('Missing SANITY_API_READ_TOKEN')
 }
 
-experimental_taintUniqueValue(
-  'Do not pass the sanity API read token to the client.',
-  process,
-  token,
-)
+// Ensure this token is only used on the server side
+if (typeof window !== 'undefined') {
+  throw new Error('Sanity API token should not be accessed on the client side')
+}
