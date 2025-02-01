40c9792f11edc16ed76df9ad6b9ac8e25a293b9a
kev <numerized@users.noreply.github.com>
2025-02-01 04:04:22 +0100

    coral title and privacy

--- app/evaluation-handicap-relationnel/page.tsx
+++ app/evaluation-handicap-relationnel/page.tsx
@@ -65,9 +65,10 @@ function EvaluationContent() {
         <div className="flex flex-col h-full print-container">
           <div className="flex-grow">
             <div className="flex flex-col md:flex-row md:justify-between md:items-start gap-4 mb-8">
-              <h1 className="text-xl sm:text-2xl md:text-3xl font-light text-primary-coral md:max-w-[70%]">
-                Formulaire d'Évaluation du Handicap Relationnel
-              </h1>
+              <div className="md:max-w-[70%]">
+                <h1 className="text-2xl sm:text-2xl md:text-3xl font-bold text-primary-coral mb-4 leading-tight">Formulaire d'Évaluation du Handicap Relationnel</h1>
+                <p className="font-bold text-primary-forest"> 100% confidentiel : Aucune donnée n'est stockée ni conservée</p>
+              </div>
               <div className="text-right space-y-2">
                 <div className="text-sm md:text-base">
                   <span className="text-gray-600">Date : </span>
