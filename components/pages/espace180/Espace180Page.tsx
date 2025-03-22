ede1621cc0695fbf165576c8288d21d98250be63
Kev <numerized@users.noreply.github.com>
2025-03-22 14:10:44 +0100

    make the play button to the left

--- components/pages/espace180/Espace180Page.tsx
+++ components/pages/espace180/Espace180Page.tsx
@@ -340,9 +340,29 @@ export default function Espace180Page() {
                           <span className="text-white font-medium">Capsule #{capsule.id}</span>
                         </div>
                       </div>
-                      {/* Control buttons */}
-                      <div className="absolute right-4 bottom-4 z-20 flex gap-2">
-                        {/* Like button */}
+                      {/* Play button - Left side */}
+                      <div className="absolute left-4 bottom-4 z-20">
+                        {/* Play/Pause button */}
+                        <button
+                          onClick={() => togglePlay(capsule.id)}
+                          className="w-14 h-14 rounded-full bg-white/20 backdrop-blur-sm flex items-center justify-center transition-all hover:bg-white/30 cursor-pointer"
+                          aria-label={activeMedia === capsule.id ? 'Pause media' : 'Play media'}
+                        >
+                          <div className="w-6 h-6 flex items-center justify-center">
+                            {activeMedia === capsule.id ? (
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
+                                <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7 0a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75h-1.5a.75.75 0 01-.75-.75V5.25z" clipRule="evenodd" />
+                              </svg>
+                            ) : (
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
+                                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
+                              </svg>
+                            )}
+                          </div>
+                        </button>
+                      </div>
+                      {/* Like button - Right side */}
+                      <div className="absolute right-4 bottom-4 z-20">
                         <div className="w-14 h-14 rounded-full relative">
                           <button
                             onClick={() => {
@@ -363,24 +383,6 @@ export default function Espace180Page() {
                             </div>
                           </button>
                         </div>
-                        {/* Play/Pause button */}
-                        <button
-                          onClick={() => togglePlay(capsule.id)}
-                          className="w-14 h-14 rounded-full bg-white/20 backdrop-blur-sm flex items-center justify-center transition-all hover:bg-white/30 cursor-pointer"
-                          aria-label={activeMedia === capsule.id ? 'Pause media' : 'Play media'}
-                        >
-                          <div className="w-6 h-6 flex items-center justify-center">
-                            {activeMedia === capsule.id ? (
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
-                                <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7 0a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75h-1.5a.75.75 0 01-.75-.75V5.25z" clipRule="evenodd" />
-                              </svg>
-                            ) : (
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
-                                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
-                              </svg>
-                            )}
-                          </div>
-                        </button>
                       </div>
                     </>
                   )}
