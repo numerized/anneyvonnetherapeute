37bce7954ecabb7e862bd9331b627b7485a2c1c4
Kev <numerized@users.noreply.github.com>
2025-03-22 15:24:06 +0100

    minor layout changes

--- app/espace180/capsule/[id]/page.tsx
+++ app/espace180/capsule/[id]/page.tsx
@@ -19,16 +19,18 @@ export default function CapsulePage() {
 
   return (
     <div>
-      <div className="container mx-auto px-4 py-4">
-        <Link 
-          href="/espace180" 
-          className="inline-flex items-center text-white bg-primary-dark/30 backdrop-blur-sm px-4 py-2 rounded-lg hover:bg-primary-dark/50 transition-all"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
-            <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
-          </svg>
-          Retour à toutes les capsules
-        </Link>
+      <div className="bg-primary-forest py-4">
+        <div className="container mx-auto px-4">
+          <Link 
+            href="/espace180" 
+            className="inline-flex items-center text-white bg-[rgb(232,146,124)] px-4 py-2 rounded-lg hover:bg-[rgb(232,146,124)]/80 transition-all"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
+              <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
+            </svg>
+            Retour à toutes les capsules
+          </Link>
+        </div>
       </div>
       <Espace180Page />
     </div>
