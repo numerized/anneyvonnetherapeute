5f37e498157c5d45cec18db05708572ca1c64e3a
Kev <numerized@users.noreply.github.com>
2025-04-19 10:59:48 +0200

    Add primary-gold color to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,6 @@ const config = {
       },
       colors: {
         'primary-gold': '#ffbe00',
-        'primary-gold': '#D9B70D',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
